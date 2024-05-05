140999ed6a2c95389bed854080a70e8bd8974b1d
Abhisek Adhikari <abhisekadhikari1906@gmail.com>
2024-05-05 17:35:32 +0530

    ✨feat: Add post count to dashboard view

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,6 +1,7 @@
 const asyncErrorHandler = require("../utils/asyncErrorHandler")
 const contactModel = require("../models/contact.model")
 const { TableBookingModel } = require("../models/table_booking.model")
+const { PostModel } = require("../models/post.model")
 
 const dashboardController = asyncErrorHandler(async (req, res) => {
     const contact = await contactModel.aggregate([
@@ -15,9 +16,16 @@ const dashboardController = asyncErrorHandler(async (req, res) => {
         },
     ])
 
+    const posts = await PostModel.aggregate([
+        {
+            $count: "posts",
+        },
+    ])
+
     res.render("index", {
         contact: contact[0]?.contacts == null ? 0 : contact[0]?.contacts,
         bookings: bookings[0]?.bookings == null ? 0 : bookings[0]?.bookings,
+        posts: posts[0]?.posts == null ? 0 : posts[0]?.posts,
     })
 })
 
